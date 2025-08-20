af6b3535d56cf76303a3daaa8d69191c70ee1043
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-08-20 15:09:26 +0530

    Remove button component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,5 +6,4 @@ export { TableGrid } from "./TableGrid1";
 export { CasinoCard } from "./cards/CasinoCard";
 
 // UI Components
-export { Button } from "./ui/Button";
 export { Card, CardContent } from "./ui/Card";
