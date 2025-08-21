5a9b1bd0dd3c7e1b2c8d561ae0e23baa64cf9a0e
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-08-21 16:53:45 +0530

    Add new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,12 @@ export { TableGrid } from "./TableGrid1";
 
 // Card Components
 export { CasinoCard } from "./cards/CasinoCard";
+export { ModalCard } from "./cards/ModalCard";
 
 // UI Components
 export { Card, CardContent } from "./ui/Card";
+export { Button } from "./ui/Button";
+
+// Modal Components
+export { LoginModal } from "./modal/LoginModal";
+export { RefillModal } from "./modal/RefillModal";
